86f4e15c232480ec4ba740379467caea5b035299
Ómar Kjartan Yasin <omarkj@gmail.com>
2013-08-12 00:23:58 -0700

    Forgot a block to parse. Yay RFC. Added a README.

---
+++ README.md
@@ -0,0 +1,49 @@
+# SNI Parser
+
+## Guaranteed to work all the time!*
+
+TLS 1.0 supports Server Name Identification (SNI) and this is an attempt to
+write a SNI parser in Erlang.
+
+## But how does it work?
+
+This library is designed to parse the CLIENT HELLO packet. If the SNI extension
+is installed it will extract the host name.
+
+## Does it work?
+
+Yes and no. I haven't tested it much but it seems to work well with OpenSSL 
+s_client and Firefox. Chrome (28) doesn't work - there is a problem parsing
+the extensions. I might look into it.
+
+## Types
+
+``` erlang
+-type parse_error() :: no_tls_handshake|
+                       invalid_ssl_version|
+                       not_whole_handshake|
+                       invalid_handshake_1|
+                       invalid_handshake_2|
+                       invalid_handshake_3|
+                       invalid_session_id|
+                       no_sni_extenson|
+                       invalid_extension_block|
+                       invalid_sni.
+
+-type hello_info() :: [info()].
+
+-type info() :: {major_ssl_version|minor_ssl_version|
+                 gmt_unix_time, pos_integer()}|
+                {random, <<_:28>>}|
+                {session_id, binary()}|
+                {sni, binary()}.
+```
+
+## API
+
+``` erlang
+-spec parse(binary()) -> {error, parse_error()}|
+                         {ok, hello_info()}.
+```
+
+* Not guaranteed at all
--- src/sni_parse.erl
+++ src/sni_parse.erl
@@ -2,43 +2,73 @@
 
 -export([parse/1]).
 
--define(TLS_HEADER_LENGTH, 5).
 -define(CONTENT_TYPE, 22).
--define(TLS_MSG_TYPE, 8#01).
+-define(MSG_TYPE, 1).
+-define(SNI_EXTENSION, 0).
 
+-type parse_error() :: no_tls_handshake|
+                       invalid_ssl_version|
+                       not_whole_handshake|
+                       invalid_handshake_1|
+                       invalid_handshake_2|
+                       invalid_handshake_3|
+                       invalid_session_id|
+                       no_sni_extenson|
+                       invalid_extension_block|
+                       invalid_sni.
+
+-type hello_info() :: [info()].
+
+-type info() :: {major_ssl_version|minor_ssl_version|
+                 gmt_unix_time, pos_integer()}|
+                {random, <<_:28>>}|
+                {session_id, binary()}|
+                {sni, binary()}.
+
+-spec parse(binary()) -> {error, parse_error()}|
+                         {ok, hello_info()}.
 parse(<<?CONTENT_TYPE, Rest/binary>>) ->
     parse_ssl_version(Rest);
 parse(Packet) ->
     {error, {no_tls_handshake, Packet}}.
 
-parse_ssl_version(<<MajorSSLVersion:8/integer-big-unsigned,
-                    MinorSSLVersion:8/integer-big-unsigned, Rest/binary>>) when
-      MajorSSLVersion =< 3 andalso
-      MinorSSLVersion =< 1 ->
-    parse_length(Rest, [{major_ssl_version, MajorSSLVersion},
-                        {minor_ssl_version, MinorSSLVersion}]);
+parse_ssl_version(<<MajorVersion:8/integer-big-unsigned,
+                    MinorVersion:8/integer-big-unsigned, 
+                    Rest/binary>>) when
+      MajorVersion =< 3 andalso
+      MinorVersion =< 1 ->
+    parse_length(Rest, [{major_ssl_version, MajorVersion},
+                        {minor_ssl_version, MinorVersion}]);
 parse_ssl_version(_) ->
     {error, invalid_ssl_version}.
 
-parse_length(<<Length:16/integer-unsigned,
-               Rest/binary>>, 
-             Retval) when Length =:= size(Rest) ->
-    parse_version(Rest, Retval);
+% Finishing up this Fragment, data left is the handshake
+parse_length(<<Length:16/integer-big-unsigned,
+               Rest/binary>>, Retval) when 
+      Length =:= size(Rest) ->
+    parse_handshake_type(Rest, Retval);
 parse_length(_, _) ->
     {error, not_whole_handshake}.
 
-parse_version(<<MajorTLSVersion:8/unsigned-big-integer,
-                MinorTLSVersion:8/unsigned-big-integer,
-                _Random:32/binary,
+parse_handshake_type(<<1:8/integer,
+                       _Length:24/unsigned-big-integer,
+                       Rest/binary>>, Retval) ->
+    % @todo check length
+    parse_version(Rest, Retval).
+
+parse_version(<<_MajorVersion:8/unsigned-big-integer,
+                _MinorVersion:8/unsigned-big-integer,
+                GMTUnixTime:32/unsigned-big-integer,
+                Random:28/binary,
                 Rest/binary>>, Retval) ->
-    parse_session(Rest, [{major_tls_version, MajorTLSVersion},
-                         {minor_tls_version, MinorTLSVersion}|Retval]);
+    parse_session(Rest, [{gmt_unix_time, GMTUnixTime},
+                         {random, Random}|Retval]);
 parse_version(_, _) ->
-    {error, invalid_tls_version}.
+    {error, invalid_handshake_1}.
 
 parse_session(<<SessionIdLength:8/unsigned-big-integer,
                 SessionId:SessionIdLength/binary,
-                Rest/binary>>, Retval) ->
+                Rest/binary>>, Retval) when SessionIdLength =< 32 ->
     parse_until_extensions(Rest, [{session_id, SessionId}|Retval]);
 parse_session(_, _) ->
     {error, invalid_session_id}.
@@ -50,28 +80,32 @@ parse_until_extensions(<<CipherSuitesLength:16/unsigned-big-integer,
                          Rest/binary>>, Retval) ->
     parse_extensions(Rest, Retval);
 parse_until_extensions(_, _) ->
-    {error, invalid_handshake_1}.
-
-parse_extensions(<<_ExtLength:16/unsigned-big-integer,
-                   Rest/binary>>, Retval) ->
-    parse_extension(Rest, Retval);
-parse_extensions(_, _) ->
     {error, invalid_handshake_2}.
 
+parse_extensions(<<ExtLength:16/unsigned-big-integer,
+                   Extensions:ExtLength/binary,
+                   _/binary>>, Retval) ->
+    parse_extension(Extensions, Retval);
+parse_extensions(_, _) ->
+    {error, invalid_handshake_3}.
+
 parse_extension(<<>>, _) ->
-    {error, no_extensons};
-parse_extension(<<12:16/unsigned-big-integer,
+    {error, no_sni_extenson};
+parse_extension(<<12:8/integer,
                   SNILength:16/unsigned-big-integer,
                   SNIPart:SNILength/binary,
                   _Rest/binary>>, Retval) ->
     parse_sni(SNIPart, Retval);
-parse_extension(<<_:16/unsigned-big-integer,
-                   Length:16/unsigned-big-integer,
-                   _:Length/binary,
-                   Rest/binary>>, Retval) ->
-    parse_extension(Rest, Retval).
+parse_extension(<<_:8/integer,
+                  Length:16/unsigned-big-integer,
+                  _Data:Length/binary,
+                  Rest/binary>>, Retval) ->
+    parse_extension(Rest, Retval);
+parse_extension(_, _) ->
+    {error, invalid_extension_block}.
 
-parse_sni(<<0:8/unsigned-big-integer,
+
+parse_sni(<<?SNI_EXTENSION:8/unsigned-big-integer,
             SNIHostLength:16/unsigned-big-integer,
             SNI:SNIHostLength/binary>>, Retval) ->
     {ok, [{sni, SNI}|Retval]};
